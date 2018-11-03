6c9445e5e41234509735410573ecf9f30f6ee21c
vishal-lia <vishallia77@gmail.com>
2018-11-03 15:14:37 +0530

    Add Comments

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.use(function(req, res, next) {
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
     res.locals.error = req.flash('error');
-    res.locals.user = req.user || null; // User obtained from Passport
+    res.locals.user = req.user || null; // Store User obtained from Passport as Global variable(used by template)
     next();
 });
 
--- config/passport.js
+++ config/passport.js
@@ -5,6 +5,8 @@ const User = require('../models/user');
 module.exports = function(passport) {
     passport.use(new LocalStrategy({usernameField: 'email'}, 
     (email, password, done) => {
+
+
         // Match User
         User.findOne({
             email: email
@@ -18,7 +20,7 @@ module.exports = function(passport) {
                 if(err) throw err;
 
                 if(isMatch) {
-                    return done(null, user); // User stored as global variable for templates
+                    return done(null, user); // Send User as param in authenticate callback (internally used by req.login)
                 } else {
                     return done(null, false, {message: 'Password Incorrect'});
                 }
@@ -27,13 +29,17 @@ module.exports = function(passport) {
     }));
 
     // Sessions
+
+    // saves the user id in the request object as request.session.passport
+    // adds the user object to the request object as request.user
     passport.serializeUser(function(user, done) {
-        done(null, user.id);
+        done(null, user.id);  
     });
-      
+    
+    // called by req.isAuthenticated() 
     passport.deserializeUser(function(id, done) {
         User.findById(id, function(err, user) {
-          done(err, user);  // User stored as global variable for templates
+          done(err, user);  
         });
     });
 }
